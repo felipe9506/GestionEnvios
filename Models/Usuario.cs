52c012a4f7a60ed63a4c1e2f4a27768957515cc4
felipe9506 <38227747+felipe9506@users.noreply.github.com>
2026-05-23 18:38:10 -0500

    Agregar archivos de proyecto.

---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace GestionEnvios.Models
+{
+    //clase para representar un usuario
+    public class Usuario
+    {
+        //propiedades del usuario
+        public int Id { get; set; }
+        public String Username { get; set; } = string.Empty;
+        public String Password { get; set; } = string.Empty;
+
+    }
+}
---
+++ Models/shipment.cs
@@ -0,0 +1,19 @@
+﻿namespace GestionEnvios.Models
+{
+    //clase para representar un envío
+    public class shipment
+    {
+        //propiedades del envío
+        public int Id { get; set; }
+        public String TrackingNumber { get; set; }
+        public String PaisOrigen { get; set; }
+        public String PaisDestino { get; set; }
+        public String CiudadOrigen { get; set; }
+        public String CiudadDestino { get; set; }
+        public String DescripcioMercancia { get; set; }
+        public decimal Peso { get; set; }
+        public String Estado { get; set; } = "Creado";
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        public DateTime FechaEstimadaEntrega { get; set; }
+    }
+}
